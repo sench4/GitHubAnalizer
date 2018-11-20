9fda3c0038ac75e18c02fb6c96cfa18802c2b80a
sench4 <alexanyan.arsen@gmail.com>
2018-11-20 20:51:05 +0400

    Some UI and backend configuration setup

---
+++ src/main/java/com/webbfontaine/githubanalizer/client/GitHubClient.java
@@ -0,0 +1,7 @@
+package com.webbfontaine.githubanalizer.client;
+
+/**
+ * Created by Arsen Aleksanyan on 11/20/18.
+ */
+public class GitHubClient {
+}
---
+++ src/main/java/com/webbfontaine/githubanalizer/config/RestAppConfig.java
@@ -0,0 +1,7 @@
+package com.webbfontaine.githubanalizer.config;
+
+/**
+ * Created by Arsen Aleksanyan on 11/20/18.
+ */
+public class AppConfig {
+}
