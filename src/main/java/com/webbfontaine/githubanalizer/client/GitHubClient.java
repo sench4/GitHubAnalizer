557ac2af96568b1cfb514f54b5a0d7fd5a52b300
sench4 <alexanyan.arsen@gmail.com>
2018-11-20 20:52:15 +0400

    Some UI and backend configuration setup

--- src/main/java/com/webbfontaine/githubanalizer/client/GitHubClient.java
+++ src/main/java/com/webbfontaine/githubanalizer/client/GitHubClient.java
@@ -1,7 +1,22 @@
 package com.webbfontaine.githubanalizer.client;
 
+import com.webbfontaine.githubanalizer.config.GitHubApiConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+
 /**
  * Created by Arsen Aleksanyan on 11/20/18.
  */
+@Service
 public class GitHubClient {
+
+    @Autowired private GitHubApiConfiguration ghApiConfig;
+
+
+
+    private String buildGitHubUrl(final String path){
+        return ghApiConfig.getGithubApiUrl() + "/" + path;
+    }
 }
--- src/main/java/com/webbfontaine/githubanalizer/config/RestAppConfig.java
+++ src/main/java/com/webbfontaine/githubanalizer/config/RestAppConfig.java
@@ -1,7 +1,20 @@
 package com.webbfontaine.githubanalizer.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
 /**
  * Created by Arsen Aleksanyan on 11/20/18.
  */
-public class AppConfig {
+@Configuration
+public class RestAppConfig {
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setSupportedMediaTypes(MediaType.parseMediaTypes("application/json;charset=UTF-8"));
+        return converter;
+    }
 }
